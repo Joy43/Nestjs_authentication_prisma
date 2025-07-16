a3a7e14ed4762c4c876c89b9c1811641f4320a20
Joy43 <ssjoy43@gmail.com>
2025-07-17 05:52:58 +0600

    user model create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [UsersModule, DatabaseModule, ProductsModule],
+  imports: [ DatabaseModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
