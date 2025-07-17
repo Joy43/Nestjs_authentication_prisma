1f4d3e8446f95c9716c51f2ea953766cf5a1d88e
Joy43 <ssjoy43@gmail.com>
2025-07-17 06:29:32 +0600

    user model creat commone

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { CommonService } from './common/common.service';
 
 
 @Module({
-  imports: [ DatabaseModule, ProductsModule, UsersModule],
+  imports: [ DatabaseModule, ProductsModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, CommonService],
 })
 export class AppModule {}
